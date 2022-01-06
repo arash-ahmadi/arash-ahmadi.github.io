7c332a6455c187a3025837c4bd2334f8d3ba8fb5
Arash <alyanq117@gmail.com>
2022-01-06 16:00:43 +0400

    Hide 2nd btn

--- js/embed.js
+++ js/embed.js
@@ -1,6 +1,7 @@
 let html = '<iframe id="receiver" class="chatbox" allowtransparency: true; style = "position: fixed; z-index: 1310; bottom: 0px; right: 0px; border: 0px; width: 400px; height: 100vh; overflow-x: hidden; overflow-y: auto;" src = "https://viubox.herokuapp.com"></iframe >';
     document.body.innerHTML += html;
     $("#receiver").on("load",function(){
+        $(".three_d_viewer_btn").css('display', 'none');
         $(this).width(400);
         var receiver = document.getElementById('receiver').contentWindow;
         // localStorage.setItem('receiver', receiver)
