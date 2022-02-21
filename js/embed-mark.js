2a6226e66c154e6a21a30839ff5b7c88b6c4cd52
Arash <alyanq117@gmail.com>
2022-02-21 11:50:01 +0400

    Fixed Marks code again'

--- js/embed-mark.js
+++ js/embed-mark.js
@@ -44,23 +44,6 @@ $('#receiver').on('load', function () {
     receiver.postMessage(message, '*');
   });
 
-  // Marks Instruction btn
-  $('#instructions_btn').on('click', function () {
-    $('#receiver').css('right', '0px');
-    $('#receiver').css('transition', 'all 0.5s');
-    var message = { message: 'virtual-dress-view-instruction' };
-    receiver.postMessage(message, '*');
-  });
-
-  // Same code for the second button which is currently hidden
-  // $('.three_d_viewer_btn').on('click', function () {
-  //   var sku = $('.measurments_btn').data('sku');
-  //   const myiframe = document.getElementById('receiver');
-  //   var message = { message: 'open-three-d-viewer-box', productSku: sku };
-  //   console.log('Open 3D  ---', message);
-  //   receiver.postMessage(message, '*');
-  // });
-
   //hover effect
   $('#iconbtn').hover(
     function () {
@@ -74,95 +57,94 @@ $('#receiver').on('load', function () {
   );
 
   // This loop runs when the client window receives a message
-  window.onmessage = function (event) {
+  window.addEventListener('message', function (event) {
     event.preventDefault();
 
     // change the width based on state of the app
-    if (event.data.message == 'Close App') {
+    if (event.data && event.data.message == 'Close App') {
       // $('#receiver').css('width', '0px');
       $('#receiver').css('right', '-400px');
       $('#receiver').css('height', '100vh');
       $('#receiver').css('transition', 'all 0.5s');
       // $('.measurments_btn').css('background-color', '#000000');
-    } else if (event.data.message == 'virtual-dress-view-open') {
+    } else if (event.data && event.data.message == 'virtual-dress-view-open') {
       // $('#receiver').css('width', '400px');
       $('#receiver').css('right', '0px');
       $('#receiver').css('height', '100vh');
       $('#receiver').css('transition', 'all 0.5s');
-    } else if (event.data.message == 'virtual-dress-view-instruction') {
-        // $('#receiver').css('width', '400px');
-        $('#receiver').css('right', '0px');
-        $('#receiver').css('height', '100vh');
-        $('#receiver').css('transition', 'all 0.5s');
-
-      // $('#receiver').css('overflow-y', 'auto')
-      // $('#receiver').width(145);
-      // $('#receiver').height(85);
-      // $('#receiver').bottom(450);
-
-      // You can ignore code below this line
+    } else if (event.data && event.data.message === 'addToken') {
+      localStorage.setItem('authToken', event.data.authToken);
+    } else if (event.data && event.data.message === 'removeToken') {
+      localStorage.removeItem('authToken');
     }
-    // else if (event.data == 'virtual-dress-view-clear-box') {
-    //   $('.measurments_btn').css('background-color', '#6f928a');
-    // } else if (event.data.message === 'viubox-recommended-size') {
-    //   $('.measurments_btn').text('Recommended Size : ' + event.data.size);
-    //   $('.measurments_btn').css('background-color', '#4CAF50');
-    // } else if (event.data.message === 'viubox-three-d-viewer-item') {
-    //   var itemId = event.data.itemId;
-    //   localStorage.setItem('itemId', itemId);
-    //   var loaded = localStorage.getItem('loaded');
-    //   if (loaded) {
-    //     var msg = { message: 'load-item', itemid: itemId };
-    //     var receiver = document.getElementById('receiver').contentWindow;
-    //     receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
-    //   }
-    // } else if (event.data.message === 'viubox-login') {
-    //   console.log('viubox-login called ');
-    //   var id = event.data.id;
-    //   localStorage.setItem('id', id);
-    //   var msg = { message: 'login', id: id };
-    //   var receiver = document.getElementById('receiver').contentWindow;
-    //   receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
-    // } else if (event.data == 'loaded-threed-viewer') {
-    //   var id = localStorage.getItem('id');
-    //   console.log('loaded-threed-viewer called ..');
-    //   var msg = { message: 'login', id: id };
-    //   var receiver = document.getElementById('receiver').contentWindow;
-    //   receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
-    // } else if (event.data == 'loaded-avatar') {
-    //   var itemId = localStorage.getItem('itemId');
-    //   var loaded = localStorage.getItem('loaded');
-    //   localStorage.setItem('loaded', true);
-    //   console.log('loaded avatar');
-    //   if (itemId) {
-    //     console.log('Calling cloth change for item id -->', itemId);
-    //     var msg = { message: 'load-item', itemid: itemId };
-    //     var receiver = document.getElementById('receiver').contentWindow;
-    //     receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
-    //   }
-    // } else if (event.data == 'loaded-app') {
-    //   var id = localStorage.getItem('id');
-    //   console.log(' loaded-app login called  called ..');
-    //   if (id) {
-    //     var msg = { message: 'login', id: id };
-    //     var receiver = document.getElementById('receiver').contentWindow;
-    //     receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
-    //   }
-    // } else if (event.data == 'virtual-three-d-hide') {
-    //   console.log('virtual-three-d-hide --> ', event.data);
-    //   $('.three_d_viewer_btn').css({ display: 'none' });
-    // } else if (event.data == 'clear-browser-query-params') {
-    //   window.history.pushState({}, document.title, '');
-    // } else if (event.data.message === 'open-three-d-app') {
-    //   var app_url = event.data.url;
-    //   console.log('Open 3d view app url', app_url);
-    //   if (app_url) {
-    //     window.location.href = app_url;
-    //   }
-    // } else {
-    //   console.log('unknown msg received --> ', event.data);
-    // }
-  };
+  });
+
+  // $('#receiver').css('overflow-y', 'auto')
+  // $('#receiver').width(145);
+  // $('#receiver').height(85);
+  // $('#receiver').bottom(450);
+
+  // You can ignore code below this line
+  // else if (event.data == 'virtual-dress-view-clear-box') {
+  //   $('.measurments_btn').css('background-color', '#6f928a');
+  // } else if (event.data.message === 'viubox-recommended-size') {
+  //   $('.measurments_btn').text('Recommended Size : ' + event.data.size);
+  //   $('.measurments_btn').css('background-color', '#4CAF50');
+  // } else if (event.data.message === 'viubox-three-d-viewer-item') {
+  //   var itemId = event.data.itemId;
+  //   localStorage.setItem('itemId', itemId);
+  //   var loaded = localStorage.getItem('loaded');
+  //   if (loaded) {
+  //     var msg = { message: 'load-item', itemid: itemId };
+  //     var receiver = document.getElementById('receiver').contentWindow;
+  //     receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
+  //   }
+  // } else if (event.data.message === 'viubox-login') {
+  //   console.log('viubox-login called ');
+  //   var id = event.data.id;
+  //   localStorage.setItem('id', id);
+  //   var msg = { message: 'login', id: id };
+  //   var receiver = document.getElementById('receiver').contentWindow;
+  //   receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
+  // } else if (event.data == 'loaded-threed-viewer') {
+  //   var id = localStorage.getItem('id');
+  //   console.log('loaded-threed-viewer called ..');
+  //   var msg = { message: 'login', id: id };
+  //   var receiver = document.getElementById('receiver').contentWindow;
+  //   receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
+  // } else if (event.data == 'loaded-avatar') {
+  //   var itemId = localStorage.getItem('itemId');
+  //   var loaded = localStorage.getItem('loaded');
+  //   localStorage.setItem('loaded', true);
+  //   console.log('loaded avatar');
+  //   if (itemId) {
+  //     console.log('Calling cloth change for item id -->', itemId);
+  //     var msg = { message: 'load-item', itemid: itemId };
+  //     var receiver = document.getElementById('receiver').contentWindow;
+  //     receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
+  //   }
+  // } else if (event.data == 'loaded-app') {
+  //   var id = localStorage.getItem('id');
+  //   console.log(' loaded-app login called  called ..');
+  //   if (id) {
+  //     var msg = { message: 'login', id: id };
+  //     var receiver = document.getElementById('receiver').contentWindow;
+  //     receiver.postMessage(msg, 'https://viubox-syz3.herokuapp.com/');
+  //   }
+  // } else if (event.data == 'virtual-three-d-hide') {
+  //   console.log('virtual-three-d-hide --> ', event.data);
+  //   $('.three_d_viewer_btn').css({ display: 'none' });
+  // } else if (event.data == 'clear-browser-query-params') {
+  //   window.history.pushState({}, document.title, '');
+  // } else if (event.data.message === 'open-three-d-app') {
+  //   var app_url = event.data.url;
+  //   console.log('Open 3d view app url', app_url);
+  //   if (app_url) {
+  //     window.location.href = app_url;
+  //   }
+  // } else {
+  //   console.log('unknown msg received --> ', event.data);
+  // }
   // function getUrlParameter(sParam) {
   //   var sPageURL = window.location.search.substring(1),
   //     sURLVariables = sPageURL.split('&'),
@@ -222,3 +204,4 @@ $('#receiver').on('load', function () {
   //   receiver.postMessage(message, '*');
   // }
 });
+console.log(12);
