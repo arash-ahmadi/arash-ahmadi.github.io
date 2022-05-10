e3101b0fcc0ec9fb019326c888d17d442c69e4b6
Arash <alyanq117@gmail.com>
2022-05-10 16:03:16 +0400

    Updated to use fingerprinting for user authentication

--- js/embed.js
+++ js/embed.js
@@ -24,23 +24,47 @@ jQuery(document).ready(function ($) {
       },
       '*'
     );
+    // Windows Parameters
+    const screenRatio = (
+      window.screen.availWidth / window.screen.availHeight
+    ).toFixed(2);
+    const memory = navigator.deviceMemory ? navigator.deviceMemory : 32; //RAM Check
+    const cookie = navigator.cookieEnabled ? 1 : 0;
+    const pixel = window.screen.pixelDepth ? window.screen.pixelDepth : 0; // Pixel depth
+    const processors = navigator.hardwareConcurrency
+      ? navigator.hardwareConcurrency
+      : 16; // Number of processors
+    const browserLength = navigator.userAgent ? navigator.userAgent.length : 50; // length of the browser string
+    const touchSupport = navigator.maxTouchPoints
+      ? navigator.maxTouchPoints
+      : 0; // Touch support
     // Getting IP address of the user
     $.getJSON('https://api.ipify.org?format=json')
       .done(function (data) {
-        const IPAdr = data.ip;
-        console.log(IPAdr);
+        const IPAdr = `${data.ip}.${screenRatio}.${pixel}.${memory}.${browserLength}.${touchSupport}.${cookie}.${processors}`;
+        console.log(`Unlimited Requests:${IPAdr}`);
         receiver.postMessage({ message: 'userIp', publicIp: IPAdr }, '*');
       })
       .fail(function () {
-        if (localStorage.getItem('authToken')) {
-          receiver.postMessage(
-            {
-              message: 'authFromParent',
-              authToken: localStorage.getItem('authToken'),
-            },
-            '*'
-          );
-        }
+        const APIKey = 'ea67a6de99ce41339e2c7c60f68be0d7';
+        const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${APIKey}`;
+        $.getJSON(url)
+          .done(function (data) {
+            const IPAdr = `${data.ip}.${screenRatio}.${pixel}.${memory}.${browserLength}.${touchSupport}.${cookie}.${processors}`;
+            console.log(`Limited Requests:${IPAdr}`);
+            receiver.postMessage({ message: 'userIp', publicIp: IPAdr }, '*');
+          })
+          .fail(function () {
+            if (localStorage.getItem('authToken')) {
+              receiver.postMessage(
+                {
+                  message: 'authFromParent',
+                  authToken: localStorage.getItem('authToken'),
+                },
+                '*'
+              );
+            }
+          });
       });
 
     $('.measurments_btn').on('click', function () {
