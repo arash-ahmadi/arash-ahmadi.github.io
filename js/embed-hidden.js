eeb430005d07131d1c945c7eefc63a00d0886869
Hamza Ahmad <hamzahmd.dev@gmail.com>
2022-09-30 19:36:18 +0500

    Added Event handlers and message for deviceOrientation

--- js/embed-hidden.js
+++ js/embed-hidden.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function ($) {
           // document.body.innerHTML += html;
           $('body').append(html);
 
-          let overlay = "<div class='viubox-syz-overlay'></div>"
+          let overlay = "<div class='viubox-syz-overlay'></div>";
           $('body').append(overlay);
           document.querySelector('.viubox-syz-overlay').style.transition =
             'all 0.5s ease-in-out';
@@ -22,14 +22,13 @@ jQuery(document).ready(function ($) {
           $('#receiver').on('load', function () {
             const enableScroll = () => {
               document.querySelector('body').style.overflow = 'auto';
-              $(".viubox-syz-overlay").css("visibility", "hidden")
-              $(".viubox-syz-overlay").css("opacity", 0)
+              $('.viubox-syz-overlay').css('visibility', 'hidden');
+              $('.viubox-syz-overlay').css('opacity', 0);
             };
             const disableScroll = () => {
               document.querySelector('body').style.overflow = 'hidden';
-              $(".viubox-syz-overlay").css("visibility", "visible")
-              $(".viubox-syz-overlay").css("opacity", 1)
-
+              $('.viubox-syz-overlay').css('visibility', 'visible');
+              $('.viubox-syz-overlay').css('opacity', 1);
             };
             // Redundant
             $(this).width('400px');
@@ -49,6 +48,51 @@ jQuery(document).ready(function ($) {
               },
               '*'
             );
+
+            // when webcam is opened to detect the orientation
+
+            const handleOrientation = (e) => {
+              receiver.postMessage(
+                {
+                  message: 'orientation',
+                  orientation: {
+                    alpha: Math.round(e.alpha),
+                    beta: Math.round(e.beta),
+                    gamma: Math.round(e.gamma),
+                  },
+                },
+                '*'
+              );
+            };
+
+            const openWebcamRequest = async () => {
+              if (localStorage.getItem('webcam') === 'true') {
+                if (
+                  typeof DeviceOrientationEvent.requestPermission === 'function'
+                ) {
+                  DeviceOrientationEvent.requestPermission()
+                    .then((response) => {
+                      if (response === 'granted') {
+                        window.addEventListener(
+                          'deviceorientation',
+                          handleOrientation
+                        );
+                      }
+                    })
+                    .catch((error) =>
+                      console.log(
+                        'DeviceOrientationEvent.requestPermission error:',
+                        error
+                      )
+                    );
+                } else {
+                  window.addEventListener(
+                    'deviceorientation',
+                    handleOrientation
+                  );
+                }
+              }
+            };
             // checking lang
             if (localStorage.getItem('lang')) {
               receiver.postMessage(
@@ -203,6 +247,15 @@ jQuery(document).ready(function ($) {
                 localStorage.setItem('authToken', event.data.authFromParent);
               } else if (event.data && event.data.message === 'changeLang') {
                 localStorage.setItem('lang', event.data.flag);
+              } else if (event.data && event.data.message === 'openWebcam') {
+                localStorage.setItem('webcam', 'true');
+                openWebcamRequest();
+              } else if (event.data && event.data.message === 'closeWebcam') {
+                localStorage.setItem('webcam', 'false');
+                window.removeEventListener(
+                  'deviceorientation',
+                  handleOrientation
+                );
               }
               // else if (event.data && event.data.message === 'ipTokenExists') {
               //   if (localStorage.getItem('authToken')) {
